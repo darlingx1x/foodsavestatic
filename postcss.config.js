5b6da4a38e6780a93ccf9f2fcacfb88ff0e20a2d
darlingx1x <warhorr2@mail.ru>
2025-09-13 14:55:39 +0500

    Initial import (static build-ready)

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+export default {
+=======
+<<<<<<< HEAD
+module.exports = {
+=======
+export default {
+>>>>>>> ce91b62 (Обновления: <кратко что поменял>)
+>>>>>>> 8fc9260 (Обновления: <кратко что поменял>)
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
